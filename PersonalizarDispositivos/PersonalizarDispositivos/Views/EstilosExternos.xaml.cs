20e41d7b280ec3bc5cb1da3e1d23d3ea08526ca8
serraguti <pacoserranox@gmail.com>
2021-05-19 11:03:01 +0200

    commit

--- PersonalizarDispositivos/PersonalizarDispositivos/Views/EstilosExternos.xaml.cs
+++ PersonalizarDispositivos/PersonalizarDispositivos/Views/EstilosExternos.xaml.cs
@@ -15,6 +15,21 @@ namespace PersonalizarDispositivos.Views
         public EstilosExternos()
         {
             InitializeComponent();
+            Label lbl = new Label
+            {
+                Text = "Piensa Macfly, piensa...",
+                FontFamily = Device.RuntimePlatform == Device.Android ?
+                "BTTF.ttf#Back to the future 2002" : null
+            };
+            this.contenedor.Children.Add(lbl);
+            Label lbl2 = new Label
+            {
+                FontSize = 100,
+                Text = "Disney Pixar",
+                FontFamily = Device.RuntimePlatform == Device.Android ?
+                "Disney family 1.ttf#Disney family 1" : null
+            };
+            this.contenedor.Children.Add(lbl2);
         }
     }
 }
